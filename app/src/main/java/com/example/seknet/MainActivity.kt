fd8ab420dc73ab6ce13d8e1e1d4ea2d2f4227695
AphidRS <aphidrs@gmail.com>
2023-04-20 19:12:04 +0200

    Navigation menu added (not functional) Appbar added

--- app/src/main/java/com/example/seknet/MainActivity.kt
+++ app/src/main/java/com/example/seknet/MainActivity.kt
@@ -1,27 +1,25 @@
 package com.example.seknet
 import android.os.Bundle
-import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.seknet.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-private lateinit var binding: ActivityMainBinding
+    private lateinit var drawerLayout : DrawerLayout
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+         binding = ActivityMainBinding.inflate(layoutInflater)
+         setContentView(binding.root)
 
-     binding = ActivityMainBinding.inflate(layoutInflater)
-     setContentView(binding.root)
+        setSupportActionBar(binding.toolbar);
+        val toggle = ActionBarDrawerToggle(this,binding.lateralMenu,binding.toolbar,
+            R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
 
      }
