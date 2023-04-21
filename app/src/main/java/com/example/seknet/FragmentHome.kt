7ae924f18f2c59b46824906dfbb7cf430201f784
AphidRS <aphidrs@gmail.com>
2023-04-21 23:22:25 +0200

    Navigation menu seknet logo added Fragment structure added

---
+++ app/src/main/java/com/example/seknet/FragmentHome.kt
@@ -0,0 +1,12 @@
+package com.example.seknet
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class FragmentHome : Fragment(R.layout.fragment_home) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        /*val someInt = requireArguments().getInt("some_int")*/
+
+    }
+}
--- app/src/main/java/com/example/seknet/MainActivity.kt
+++ app/src/main/java/com/example/seknet/MainActivity.kt
@@ -3,7 +3,13 @@ import android.os.Bundle
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import androidx.fragment.app.findFragment
+import androidx.navigation.Navigation
 import com.example.seknet.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationBarMenu
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,12 +20,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
          binding = ActivityMainBinding.inflate(layoutInflater)
          setContentView(binding.root)
+            setSupportActionBar(binding.toolbar)
 
-        setSupportActionBar(binding.toolbar);
-        val toggle = ActionBarDrawerToggle(this,binding.lateralMenu,binding.toolbar,
-            R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
+            drawerLayout = binding.lateralMenu
+            val toggle = ActionBarDrawerToggle(this,binding.lateralMenu,binding.toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+            drawerLayout.addDrawerListener(toggle);
+            toggle.syncState();
+
+            if (savedInstanceState == null) {
+                supportFragmentManager.commit {
+                    setReorderingAllowed(true)
+                    add<FragmentHome>(R.id.fragmentContainer)
+                }
+            }
 
 
      }
