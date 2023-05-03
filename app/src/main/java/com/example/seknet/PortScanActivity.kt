348ee2d331604e1bb9ef824454657ab032168161
FeXeTe <fexete@live.com>
2023-05-03 18:49:17 +0200

    portscanner running

---
+++ app/src/main/java/com/example/seknet/PortScanActivity.kt
@@ -0,0 +1,11 @@
+package com.example.seknet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class portScanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_port_scan)
+    }
+}
