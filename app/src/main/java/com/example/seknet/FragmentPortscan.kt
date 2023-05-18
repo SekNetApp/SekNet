c5886cee0e43a41e9b0aa17a6bf428b67588312c
AphidRS <aphidrs@gmail.com>
2023-05-19 00:27:04 +0200

    Portscan logic added

--- app/src/main/java/com/example/seknet/FragmentPortscan.kt
+++ app/src/main/java/com/example/seknet/FragmentPortscan.kt
@@ -3,90 +3,87 @@ package com.example.seknet
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.example.seknet.databinding.FragmentPortscanBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import org.json.JSONObject.NULL
 import java.io.IOException
 import java.net.InetSocketAddress
 import java.net.Socket
 
+
 class FragmentPortscan : Fragment(R.layout.fragment_portscan) {
 
     private lateinit var binding: FragmentPortscanBinding
-    private lateinit var btnScan: Button
-    private lateinit var etTarget: EditText
-    private lateinit var tvResult: TextView
-    private lateinit var etStartPort: EditText
-    private lateinit var etEndPort: EditText
+    private var openPorts: ArrayList<String> = arrayListOf()
+    private lateinit var portResultList: ArrayAdapter<String>
+    private lateinit var listView: ListView
+    private lateinit var s: Socket
+    private var ip: String = ""
+    private var to: Int = 0
+    private var from: Int = 0
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentPortscanBinding.bind(view)
         activity?.title = "PORTSCAN"
-        initComponents()
+        setAdapter()
         initListeners()
-    }
-
-    private fun initComponents() {
-        btnScan = binding.btnScan
-        tvResult = binding.tvResult
-        etStartPort = binding.etStartPort
-        etEndPort = binding.etEndPort
-        etTarget = binding.etTarget
     }
 
     @SuppressLint("SetTextI18n")
     private fun initListeners() {
-        btnScan.setOnClickListener {
-            onScanButtonClicked()
+        binding.portscanButtonScan.setOnClickListener {
+            setPortParameters()
+        }
+        binding.portscanButtonClear.setOnClickListener {
+            portResultList.clear()
         }
     }
 
-    private suspend fun portScan(ipAddress: String, startPort: Int, endPort: Int): List<Int> = withContext(Dispatchers.IO) {
-        val openPorts = mutableListOf<Int>()
-        for (port in startPort..endPort) {
-            try {
-                val socket = Socket()
-                socket.connect(InetSocketAddress(ipAddress, port), 1000)
-                socket.close()
-                openPorts.add(port)
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
+    private fun setPortParameters() {
+        ip = binding.portscanTargetHost.text.toString()
+        from = binding.portscanFromPort.text.toString().toInt()
+        to = binding.portscanToPort.text.toString().toInt()
+        lifecycleScope.launch {
+            onScanButtonClicked(ip, from, to)
         }
-        return@withContext openPorts
-
     }
 
-@SuppressLint("SetTextI18n")
-private fun onScanButtonClicked() {
-    lifecycleScope.launch {
-        try {
-            val ipAddress = etTarget.toString()
-            val startPort = etStartPort.toString().toIntOrNull()
-            val endPort = etEndPort.toString().toIntOrNull()
-            val openPorts = startPort?.let {
-                if (endPort != null) {
-                    portScan(ipAddress, it, endPort)
+    private suspend fun onScanButtonClicked(ip: String, from: Int, to: Int) =
+        withContext(Dispatchers.IO) {
+            for (port in from..to) {
+                try {
+                    s = Socket()
+                    s.connect(InetSocketAddress(ip, port), 1000)
+                    if (s.isConnected) {
+                        openPorts.add("Host $ip is OPEN on port $port")
+                        requireActivity().runOnUiThread {
+                            portResultList.notifyDataSetChanged()
+                        }
+                        s.close()
+                    }
+                } catch (e: IOException) {
+                    openPorts.add("Host $ip is CLOSED on port $port")
+                    requireActivity().runOnUiThread {
+                        portResultList.notifyDataSetChanged()
+                    }
+                    s.close()
+                } catch (e: NullPointerException) {
+                    e.printStackTrace()
                 }
             }
-            if(openPorts.toString() == NULL) {
-                tvResult.text = "ALL CLOSE"
-            } else {
-                tvResult.text = "Los puertos abiertos son: " + openPorts.toString()
-            }
-
-        } catch (e: Exception) {
-            e.printStackTrace()
         }
-    }
-}
 
+    private fun setAdapter() {
+
+        portResultList = ArrayAdapter(requireActivity(), R.layout.listview_item, openPorts)
+        listView = binding.portscanResultList
+        listView.adapter = portResultList
+
+    }
 }
