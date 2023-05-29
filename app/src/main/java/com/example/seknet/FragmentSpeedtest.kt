4a8c16972c618a44c9ec373c2285e7906d6d46b6
Alvaro H <aphidrs@gmail.com>
2023-05-29 17:21:58 +0200

    Speedtest through webview public speed test web added

--- app/src/main/java/com/example/seknet/FragmentSpeedtest.kt
+++ app/src/main/java/com/example/seknet/FragmentSpeedtest.kt
@@ -1,27 +1,24 @@
 package com.example.seknet
 
-import android.content.Context
-import android.net.ConnectivityManager
-import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.webkit.WebView
 import androidx.fragment.app.Fragment
-import com.example.seknet.databinding.FragmentPortscanBinding
 import com.example.seknet.databinding.FragmentSpeedtestBinding
+
 class FragmentSpeedtest : Fragment(R.layout.fragment_speedtest) {
     private lateinit var binding : FragmentSpeedtestBinding
+    private lateinit var webView : WebView
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentSpeedtestBinding.bind(view)
         activity?.title = "SPEEDTEST"
+        setWebView()
+    }
 
-        binding.speedTestButton.setOnClickListener {
-            val cm = activity?.applicationContext?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-            val nc = cm.getNetworkCapabilities(cm.activeNetwork)
-            val downSpeed = (nc?.linkDownstreamBandwidthKbps)?.div(1000)
-            val upSpeed = (nc?.linkUpstreamBandwidthKbps)?.div(1000)
-            binding.speedTestResultsDownload.setText(downSpeed.toString())
-            binding.speedTestResultsUpload.setText(upSpeed.toString())
-        }
+    private fun setWebView(){
+        webView = binding.webWindow
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("https://speedsmart.net/")
     }
 }
